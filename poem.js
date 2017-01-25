f39388a13506edb40cd9e540a542211d874588e8
nancyitaliya <nancyitaliya97@gmail.com>
2017-01-25 16:44:03 -0500

    My Iambic Pentameter Poem

--- poem.js
+++ poem.js
@@ -1,2 +1,3 @@
-console.log('Hello World');
-console.log('Goodbye World');
+console.log('It did not go the way I planned, my god,');
+console.log('All I wanted was to eat some more food,');
+console.log('But Phil had to eat them all in one go.');
