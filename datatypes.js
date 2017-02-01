205c6c869e9c2e9a2c00a9cf4ef8e671efdb8d01
nancyitaliya <nancyitaliya97@gmail.com>
2017-02-01 13:53:10 -0500

    Data Types

---
+++ datatypes.js
@@ -0,0 +1,41 @@
+console.log ('Data Types'.toUpperCase());
+
+console.log ( )
+
+console.log ('text'.toUpperCase());
+var a = "Hello Everybody!";
+console.log (a);
+
+console.log ( )
+
+console.log ('number'.toUpperCase());
+var b = 5;
+var c = 10;
+console.log (b+c);
+
+console.log ( )
+
+console.log ('array'.toUpperCase());
+var a = ['I Like', 2, 'Code'
+];
+console.log (a)
+
+console.log ( )
+
+console.log ('boolean'.toUpperCase())
+var a = true;
+var b = false;
+
+if (a = false) {
+	console.log ('You have a problem');
+} else {
+	console.log ('You are all set!');
+}
+
+console.log ( )
+
+console.log ('undefined'.toUpperCase());
+var a ;
+console.log (a);
+
+console.log ( )
---
+++ practise.js
@@ -0,0 +1,24 @@
+
+var a = 3;
+var b = 1;
+
+if (a > 16) {
+	console.log ('Greater than 16!');
+} else {
+	console.log ('Not greater than 16!');
+}
+
+var b = ['Ratio', 1
+];
+
+console.log (b)
+
+var c = 'Hi';
+var d = 'Bye';
+var e = c +  ' ' + d
+
+console.log (c);
+
+console.log (d.toUpperCase());
+
+console.log (e);
