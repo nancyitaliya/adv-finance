321177e3de2b6c737f3acb9c2913c84a499e8c81
nancyitaliya <nancyitaliya97@gmail.com>
2017-02-08 17:08:17 -0500

    If and For Loops

---
+++ hello.js
@@ -0,0 +1,24 @@
+var d1 = 10,
+d2 = 5,
+d3 = 8;
+
+if (d1 = 10) {
+	console.log(d2)
+} else {
+	console.lof(d3)
+}
+
+var array = [1,2,3,4]
+
+console.log(array.length)
+
+array [0]
+
+console.log (array.length);
+
+i = i + 1
+
+for (var i = 0; i < array.length; i++) {
+	console.log (array[i]);
+}
+
